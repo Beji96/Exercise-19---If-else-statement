c9dbf3ac848b065de3bda9b35a29130c02631692
Flavius <flaviusbejan96@yahoo.com>
2022-09-21 10:44:06 +0300

    Finish

---
+++ exercise.js
@@ -0,0 +1,10 @@
+let myName = "Flavius";
+let surname = "Rossi";
+let fullName = "";
+
+if (myName && surname) {
+  fullName = "Flavius Rossi";
+} else {
+  fullName = "Full name is invalid";
+}
+console.log(fullName);
